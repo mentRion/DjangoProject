63b4477bd296cccd26bdc140c0568cce491c645d
ross <rectoross@gmail.com>
2022-11-07 18:15:38 +0800

    add text in response

--- hello/views.py
+++ hello/views.py
@@ -4,4 +4,14 @@ from django.http import  HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse('hello bsit')
+    return HttpResponse('<html>'
+                        '<head>'
+                        '<style>'
+                        'body{background-color:black;}'
+                        'h1{color:white;}'
+                        '</style>'
+                        '</head>'
+                        '<body>'
+                        '<h1>Hello, World</h1>'
+                        '</body>'
+                        '</html>')
